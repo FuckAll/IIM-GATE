92975b7d1c329da55808c620a7261faa2ee8f606
xiangdong.wu <xiangdong.wu@opay-inc.com>
2022-08-09 10:05:11 +0800

    git ignore

--- iim-gate-tcp/src/main/java/xyz/izgnod/iim/gate/tcp/server/TcpChannelInitializer.java
+++ iim-gate-tcp/src/main/java/xyz/izgnod/iim/gate/tcp/server/TcpChannelInitializer.java
@@ -3,6 +3,7 @@ package xyz.izgnod.iim.gate.tcp.server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.timeout.IdleStateHandler;
 import xyz.izgnod.iim.gate.core.codec.MessageCodec;
 import xyz.izgnod.iim.gate.core.config.ServerProperties;
 
@@ -17,8 +18,8 @@ public class TcpChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-//        ch.pipeline().addLast("timeout",
-//                new IdleStateHandler(0, 60, 0));
+        ch.pipeline().addLast("timeout",
+                new IdleStateHandler(0, 60, 0));
         ch.pipeline().addLast("framer",
                 new LengthFieldBasedFrameDecoder(serverProperties.getFrameMaxLength(), 16,
                         4, 0, 0, true));
